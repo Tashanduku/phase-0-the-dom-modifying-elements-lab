9397f500729420ddb96df42003b4ad647e761e50
Natasha Nduku <natashanduku0@gmail.com>
2024-11-22 13:01:17 +0300

    modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Natasha is the champion';
+
+document.body.appendChild(newHeader);
